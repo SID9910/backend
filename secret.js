25004222ecbb5e05681ab6ebbf6121453b11d6c8
SIDDHARTH GUPTA <95719152+SID9910@users.noreply.github.com>
2023-09-11 16:43:21 +0530

    backend

---
+++ secret.js
@@ -0,0 +1,4 @@
+
+const db_link ='mongodb+srv://siddharthgupta052:siddharthgupta@backend-1.k9bfvfv.mongodb.net/?retryWrites=true&w=majority';
+
+module.exports ={db_link};
--- server.js
+++ server.js
@@ -1,3 +1,49 @@
-//install nodemon
-//install lodash
-//install express
+const mongoose = require('mongoose');
+const express = require("express");
+const app = express();
+const { db_link} = require('./secret');
+// console.log(db_link);
+app.use(express.json());
+mongoose.connect(db_link)
+.then(function(db){
+    console.log("db connected");
+    // console.log(db);
+}).catch(function(err){
+    console.log(err);
+})
+
+const userSchema = mongoose.Schema({
+
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 8,
+    },
+    confirmPassword:{
+        type: String ,
+        required: true,
+        minLength: 8,
+    },
+})
+
+const userModel =mongoose.model("userModel", userSchema);
+
+(async function createUser() {
+    let user ={
+        name:"siddharth",
+        email: "siddharthgupta052@gmail.com",
+        password:"1234456677",
+        confirmPassword: "1234456677"
+    };
+    let data =await userModel.create(user);
+    console.log(data);
+})();
